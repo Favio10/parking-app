1b0892f6514285843ce3eb414f279d2c024e4b37
Favio10 <favio_olivera@hotmail.com>
2024-08-07 23:51:23 -0300

    segundo commit

--- back/src/controllers/userController.ts
+++ back/src/controllers/userController.ts
@@ -3,3 +3,10 @@ import { Request, Response } from 'express';
 export const getAllUser = (req: Request, res: Response): void => {
   res.json({ message: 'Obtener todos los usuarios' });
 };
+
+export const getUser = (req, res) => {
+  const { id } = req.params;
+  // Aquí deberías obtener un solo usuario por ID desde la base de datos
+  // Ejemplo:
+  res.json({ message: `Obtener usuario con ID: ${id}` });
+};
